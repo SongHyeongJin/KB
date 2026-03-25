1f6e3c3ee5a109656924e4f4126523aef0671dbb
SongHyeongJin <hjsong1102@gmail.com>
2026-03-25 13:09:50 +0900

    3월24일 과제

--- Practice/03-24/test-vue-app/src/main.js
+++ Practice/03-24/test-vue-app/src/main.js
@@ -1,7 +1,7 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue';
 // import App from './App.vue'
 // import App from './App2.vue'
-import App from './App4.vue'
+import App from './App4.vue';
 // import CheckBoxItem from './Components/CheckBoxItem.vue';
 // createApp(App).component('CheckboxItem', CheckBoxItem).mount('#app')
-createApp(App).mount('#app')
+createApp(App).mount('#app');
