59de47c7b763e91355b4fc158a27d9fc5e52b422
thatzcool <mycaptain0771@gmail.com>
2026-03-25 17:00:21 +0900

    vuejs03_end

--- 6_1Vuejs/vuejs_ex/vuejs03/ComponentPropsandEvents/1/valid/src/components/UserProfile.vue
+++ 6_1Vuejs/vuejs_ex/vuejs03/ComponentPropsandEvents/1/valid/src/components/UserProfile.vue
@@ -1,4 +1,15 @@
 <script>
-export default {};
+export default {
+  emits: [`print-hello`],
+  methods: {
+    parentEventCall() {
+      // this.$emit('print-hello');
+      this.$emit(`onPrint-hello`);
+    },
+  },
+};
 </script>
-<template></template>
+<template>
+  <!-- <button @click="$emit('print-hello')">클릭</button>  인라인핸들러 방식-->
+  <button @click="parentEventCall">클릭</button>
+</template>
