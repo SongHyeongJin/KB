91c0f228244407c2e4d4ae81da12b060c41c365e
SongHyeongJin <hjsong1102@gmail.com>
2026-03-25 17:51:01 +0900

    03-25 과제 기본까지 완료

---
+++ Practice/03-25/style-test/src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import Child1 from './components/Child1.vue';
+import Child2 from './components/Child2.vue';
+import Child3 from './components/Child3.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Child1,
+    Child2,
+    Child3,
+  },
+};
+</script>
+
+<template>
+  <Child1 />
+  <Child2 />
+  <Child3 />
+</template>
+
+<style></style>
