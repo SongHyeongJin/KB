d577152360b4b7db5e1b0046a8b8fd7541a36b74
thatzcool <mycaptain0771@gmail.com>
2026-04-15 14:44:26 +0900

    day02/control-statement_01

---
+++ 7_java/java_basic/src/day01/section01/Calculator.java
@@ -0,0 +1,24 @@
+//Calculator 클래스를 작성하세요
+//두개의 정수값을 더하여 결과값을 출력하는 자바 프로그램 작성
+package day01.section01;
+
+public class Calculator{
+    public static void main(String[] args) {
+        int number1;
+        int number2;
+        // int n1, n2, n3;
+
+        number1 = 10;
+        number2 = 20;
+
+        // int number1 = 10;
+
+        int result;
+
+        result = number1 + number2;
+
+        System.out.println(result);
+
+    }
+
+}
