0578b4f12bb2e68f46993fdce9884f989bdfe2ae
SongHyeongJin <hjsong1102@gmail.com>
2026-04-15 15:01:20 +0900

    ãJava 실습

---
+++ 7_java/java_basic/src/day01/section02/variable/VariableExchangeExample.java
@@ -0,0 +1,21 @@
+package day01.section02.variable;
+
+public class VariableExchangeExample {
+    public static void main(String[] args) {
+        //정수형 변수 x 에 3 값 할당
+        int x = 3;
+        //정수형 변수 y 에 5 값 할당
+        int y = 5;
+        //현재 x, y, 값 출력  x=3, y=5
+        System.out.println("x = " + x + ", y = " + y);
+        //x의 값과 y의 값을 교환 한 후 결과 출력
+        int temp = 0;
+
+        temp = x;
+        x = y;
+        y = temp;
+
+        System.out.println("x = " + x + ", y = " + y);
+
+    }
+}
